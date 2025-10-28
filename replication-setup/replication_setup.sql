c94e3abcae5103fa0e2117ab3316531247e1f0ec
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2025-10-28 15:54:25 +0000

    Feartures : Adding stream replication setup

--- replication-setup/replication_setup.sql
+++ replication-setup/replication_setup.sql
@@ -9,10 +9,15 @@ GRANT USAGE ON SCHEMA :"target_schema" TO :"sysadmin_user";
 GRANT SELECT ON ALL TABLES IN SCHEMA :"target_schema" TO :"sysadmin_user";
 ALTER DEFAULT PRIVILEGES IN SCHEMA :"target_schema" GRANT SELECT ON TABLES TO :"sysadmin_user";
 
+-- Injecter les variables côté serveur pour usage dans le DO
+SELECT set_config('mosip.rep_pub_name', :'publication_name', false);
+
 -- Créer la publication vide si absente (aucune table ajoutée par défaut)
 DO $$
+DECLARE
+    pub_name text := current_setting('mosip.rep_pub_name');
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_publication WHERE pubname = :'publication_name') THEN
-        EXECUTE format('CREATE PUBLICATION %I', :'publication_name');
+    IF NOT EXISTS (SELECT 1 FROM pg_publication WHERE pubname = pub_name) THEN
+        EXECUTE format('CREATE PUBLICATION %I', pub_name);
     END IF;
 END$$;

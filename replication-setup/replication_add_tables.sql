f68d36bbf1ec6553dbe5c59b7c93c9460c1c67e0
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2025-10-28 16:06:07 +0000

    Feartures : Adding stream replication setup

--- replication-setup/replication_add_tables.sql
+++ replication-setup/replication_add_tables.sql
@@ -3,14 +3,20 @@
 --   :publication_name  (ex: iam_pub)
 --   :table_list        (liste séparée par des virgules: iam.user_detail, iam.user_role)
 
+-- Injecter les variables côté serveur
+SELECT set_config('mosip.rep_pub_name', :'publication_name', false);
+SELECT set_config('mosip.rep_table_list', :'table_list', false);
+
 DO $$
 DECLARE
-	table_names TEXT[] := string_to_array(:'table_list', ',');
-	tbl TEXT;
+	pub_name text := current_setting('mosip.rep_pub_name');
+	raw_list text := current_setting('mosip.rep_table_list');
+	table_names text[] := regexp_split_to_array(raw_list, '\s*,\s*');
+	tbl text;
 BEGIN
 	FOREACH tbl IN ARRAY table_names LOOP
-		IF trim(tbl) <> '' THEN
-			EXECUTE format('ALTER PUBLICATION %I ADD TABLE %s', :'publication_name', trim(tbl));
+		IF tbl IS NOT NULL AND length(trim(tbl)) > 0 THEN
+			EXECUTE format('ALTER PUBLICATION %I ADD TABLE %s', pub_name, trim(tbl));
 		END IF;
 	END LOOP;
 END$$;

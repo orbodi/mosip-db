bd816fb0ea5b1f226e5c9a9aae661d60c3ef4f51
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2025-11-05 10:43:44 +0000

    Fix : mosip pms

--- database/mosip_pms/dml.sql
+++ database/mosip_pms/dml.sql
@@ -36,11 +36,14 @@ TRUNCATE TABLE pms.reg_device_type cascade ;
 TRUNCATE TABLE pms.reg_device_sub_type cascade ;
 \COPY pms.reg_device_sub_type (code,dtyp_code,name,descr,is_active,cr_by,cr_dtimes) FROM './dml/pms-reg_device_sub_type.csv' delimiter ',' HEADER  csv;
 
-TRUNCATE TABLE pms.device_detail cascade ;
-\COPY pms.device_detail (id,dprovider_id,dtype_code,dstype_code,make,model,partner_org_name,approval_status,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes) FROM './dml/pms-device_detail.csv' delimiter ',' HEADER  csv;
+-- SKIP: device_detail not present in this setup
+-- TRUNCATE TABLE pms.device_detail cascade ;
+-- \COPY pms.device_detail (id,dprovider_id,dtype_code,dstype_code,make,model,partner_org_name,approval_status,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes) FROM './dml/pms-device_detail.csv' delimiter ',' HEADER  csv;
 
-TRUNCATE TABLE pms.secure_biometric_interface cascade ;
-\COPY pms.secure_biometric_interface (id,sw_binary_hash,sw_version,sw_cr_dtimes,sw_expiry_dtimes,approval_status,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes) FROM './dml/pms-secure_biometric_interface.csv' delimiter ',' HEADER  csv;
+-- SKIP: secure_biometric_interface not present in this setup
+-- TRUNCATE TABLE pms.secure_biometric_interface cascade ;
+-- \COPY pms.secure_biometric_interface (id,sw_binary_hash,sw_version,sw_cr_dtimes,sw_expiry_dtimes,approval_status,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes) FROM './dml/pms-secure_biometric_interface.csv' delimiter ',' HEADER  csv;
 
-TRUNCATE TABLE pms.secure_biometric_interface_h cascade ;
-\COPY pms.secure_biometric_interface_h (id,sw_binary_hash,sw_version,sw_cr_dtimes,sw_expiry_dtimes,approval_status,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,eff_dtimes) FROM './dml/pms-secure_biometric_interface_h.csv' delimiter ',' HEADER  csv;
+-- SKIP: secure_biometric_interface_h not present in this setup
+-- TRUNCATE TABLE pms.secure_biometric_interface_h cascade ;
+-- \COPY pms.secure_biometric_interface_h (id,sw_binary_hash,sw_version,sw_cr_dtimes,sw_expiry_dtimes,approval_status,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,eff_dtimes) FROM './dml/pms-secure_biometric_interface_h.csv' delimiter ',' HEADER  csv;

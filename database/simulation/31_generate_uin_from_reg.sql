28e4946ca335fdc8a3db847d5da30e4d2a7282e0
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2025-11-05 12:48:38 +0000

    Features : Adding simulation scripts

--- database/simulation/31_generate_uin_from_reg.sql
+++ database/simulation/31_generate_uin_from_reg.sql
@@ -2,7 +2,7 @@
 
 -- Générer des UIN (supposés dérivés des registrations approuvées)
 -- Usage: psql -v sim_uin_from_reg=150 -f 31_generate_uin_from_reg.sql
-\set sim_uin_from_reg :sim_uin_from_reg 150
+\set sim_uin_from_reg 150
 
 DO $$
 DECLARE v_cnt int := 150; has_reg_id boolean;
@@ -18,14 +18,16 @@ BEGIN
            lpad((100000000000 + floor(random()*899999999999))::bigint::text,12,'0'),
            md5(random()::text), E'\\x'::bytea, md5(random()::text), 'REG' || to_char(now(), 'YYYYMMDDHH24MISS') || lpad(i::text,6,'0'),
            'ACTIVE','fra','sim', now()
-    FROM generate_series(1, v_cnt) AS s(i);
+    FROM generate_series(1, v_cnt) AS s(i)
+    ON CONFLICT DO NOTHING;
   ELSE
     INSERT INTO idrepo.uin (uin_ref_id, uin, uin_hash, uin_data, uin_data_hash, status_code, lang_code, cr_by, cr_dtimes)
     SELECT gen_random_uuid(),
            lpad((100000000000 + floor(random()*899999999999))::bigint::text,12,'0'),
            md5(random()::text), E'\\x'::bytea, md5(random()::text),
            'ACTIVE','fra','sim', now()
-    FROM generate_series(1, v_cnt) AS s(i);
+    FROM generate_series(1, v_cnt) AS s(i)
+    ON CONFLICT DO NOTHING;
   END IF;
 END $$;
 
